e4f1bf667f35f62be6851d98dc5a90b559f934b6
chrisunjae <lee.christophersunjae@gmail.com>
2019-04-03 01:19:08 -0400

    Finished Python to convert CSVs to JSONs

--- CSV Conversions/quote_day.py
+++ CSV Conversions/quote_day.py
@@ -31,13 +31,32 @@ def nextday(date):
         year = date[0]
         month = date[1]
         day = date[2] + 1
-    return (year, month, day)
+    return [year, month, day]
+
+
 
 with open(outpath, "r") as jsonfile:
     data = json.loads(jsonfile.read())
     dates = data.keys()
     lastday = dates[len(dates) - 1]
     temp = lastday.split(" ")
-    lastdayformat = [int(temp[0]), int(temp[1]), int(temp[2])]
-    print(nextday([2018, 2, 28]))
+    lastdayformat = [int(temp[0]), int(temp[1]), ord(temp[2])-64]
+    day = lastdayformat
+    try:
+        start = quotes.index(data[lastday][0]) + 1
+        pass
+    except:
+        start = 0
+        pass
+    
+    length = len(quotes)
+    for i in range(start, length):
+        day = nextday(day)
+        daystring = str(day[0]) + " " + str(day[1]) + " " + chr(day[2]+64)
+        data[daystring] = [quotes[i], people[i]]
+    jsonout = json.dumps(data, sort_keys=True, indent=2)
+
+with open(outpath, "w") as jsonfile:
+    jsonfile.write(jsonout)
+    
     
